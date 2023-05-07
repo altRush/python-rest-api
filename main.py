f76077973ad99edf0680373c83967d726e2b6fe2
Waraht Punyarote <rushing.wp@gmail.com>
2023-05-07 13:12:57 +0700

    feat: Search by path param and query string

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+allEvents = [{
+    "id":          1,
+    "title":       "Introduction to Python",
+    "description": "Come join us for a chance to learn how Python and FastAPI work and get to eventually try it out.",
+}, {
+    "id":          2,
+    "title":       "Introduction to Python again",
+    "description": "Come join us for a chance to learn how Python and FastAPI work and get to eventually try it out again.",
+}]
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "Hello world"
+
+
+@app.get("/event/{id}")
+def getEventById(id: int = Path(..., description="ID of the event"), gt=0):
+    for event in allEvents:
+        if event["id"] == id:
+            return event
+    return "No match event by specified ID found"
+
+
+@app.get("/get-by-title")
+def getEventByTitle(title: str):
+    for event in allEvents:
+        if event["title"] == title:
+            return event
+    return "No match event by specified title found"
